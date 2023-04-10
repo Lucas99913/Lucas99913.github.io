b429f2edcf904b74cae9b5daea02105e4012d98c
Lucas Giorsetti <lucasgiorsetti@MacBook-Pro-de-Lucas-2.local>
2023-04-10 17:41:39 +0200

    Implementation of the parallax

--- js/global.js
+++ js/global.js
@@ -174,3 +174,67 @@ const observerIntersectionAnimation = () => {
 
 observerIntersectionAnimation();
 
+/**
+ * Calcul la position de l'élément par rapport au haut de la page
+ * @param {HTMLElement} element
+ * @returns {number}
+ */
+
+function offsetTop(element, acc = 0) {
+  if (element.offsetParent) {
+    return offsetTop(element.offsetParent, acc + element.offsetTop);
+  }
+  return acc + element.offsetTop;
+}
+
+class Parallax {
+  /**
+   * @param {HTMLElement} element
+   */
+  constructor(element) {
+    this.element = element;
+    this.ratio = parseFloat(element.dataset.parallax);
+    this.onScroll = this.onScroll.bind(this);
+    this.onIntersection = this.onIntersection.bind(this);
+    this.elementY = offsetTop(this.element) + this.element.offsetHeight / 2;
+    const observer = new IntersectionObserver(this.onIntersection)
+    observer.observe(element)
+    document.addEventListener('scroll', this.onScroll)
+    this.onScroll();
+  }
+
+  /**
+   * @param {IntersectionObserverEntry[]} entries
+  */
+
+  onIntersection(entries) {
+    for (const entry of entries) {
+      if (element.isIntersecting) {
+        document.addEventListener('scroll', this.onScroll);
+      } else {
+        document.removeEventListener('scroll', this.onScroll);
+      }
+    }
+  }
+
+  onScroll() {
+    window.requestAnimationFrame(() => {
+      this.elementY = offsetTop(this.element) + this.element.offsetHeight / 2;
+      const screenY = window.scrollY + window.innerHeight / 2;
+      const diffY = this.elementY - screenY;
+      this.element.style.setProperty('transform', `translateY(${diffY * -1 * this.ratio}px)`)
+    })
+  }
+
+  /**
+   *
+   * @returns {Parallax[]}
+   */
+  static bind() {
+    return Array.from(document.querySelectorAll('[data-parallax]')).map((element) => {
+      return new Parallax(element);
+    })
+  }
+}
+
+Parallax.bind();
